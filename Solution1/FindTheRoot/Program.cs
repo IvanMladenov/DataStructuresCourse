d87567d862cf192a4cd61f0f3b081e77b36c4f8a
IvanMladenov <ismladenov@gmail.com>
2015-07-29 00:34:48 +0300

    Start commit homework first problem

---
+++ Solution1/FindTheRoot/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace FindTheRoot
+{
+    using System;
+
+    internal class Program
+    {
+        private static void Main()
+        {
+            int numberOfPairs = int.Parse(Console.ReadLine());
+            string[] pairs = new string[numberOfPairs];
+
+            for (int i = 0; i < numberOfPairs; i++)
+            {
+                int parent = int.Parse(pairs[i][0].ToString());
+                int child = int.Parse(pairs[i][2].ToString());
+            }
+        }
+    }
+}
