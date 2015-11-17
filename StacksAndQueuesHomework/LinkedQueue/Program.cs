f1d5e0be1fe506d6426a492ec665b09037bb3044
IvanMladenov <ismladenov@gmail.com>
2015-11-17 15:58:48 +0200

    LinkedQueue and unit test for it. My implementation of the structure

---
+++ StacksAndQueuesHomework/LinkedQueue/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LinkedQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var q = new LinkedQueue<int>();
+            //q.Enqueue(1);
+            //q.Enqueue(2);
+            //q.Enqueue(3);
+            //q.Enqueue(4);
+            //q.Enqueue(5);
+
+            //Console.WriteLine(string.Join(" ", q.ToArray()));
+            //while (q.Count>0)
+            //{
+            //    Console.WriteLine(q.Dequeue());
+            //}
+            
+        }
+    }
+}
---
+++ StacksAndQueuesHomework/LinkedQueueTests/TestMethods.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using LinkedQueue;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LinkedQueueTests
+{
+    [TestClass]
+    public class TestMethods
+    {
+        [TestMethod]
+        public void TestEnqueueDequeueSingleElementShouldChangeCountDequeueTheSameElementLikeEnqueueed()
+        {
+            LinkedQueue<int> q = new LinkedQueue<int>();
+            int number = 5;
+
+            int count = q.Count;
+            Assert.AreEqual(count, 0);
+
+            q.Enqueue(number);
+            Assert.AreEqual(1, q.Count);
+
+            int Dequeueed = q.Dequeue();
+            Assert.AreEqual(number, Dequeueed);
+
+            Assert.AreEqual(0, q.Count);
+        }
+
+        [TestMethod]
+        public void TestEnqueueDequeueTousendElementsTheCountShouldBeCorrectIndirectlyTestAutoGrow()
+        {
+            LinkedQueue<int> q = new LinkedQueue<int>();
+
+            for (int i = 1; i <= 1000; i++)
+            {
+                Assert.AreEqual(i - 1, q.Count);
+                q.Enqueue(i);
+            }
+
+            for (int i = 1000; i >= 1; i--)
+            {
+                Assert.AreEqual(i, q.Count);
+                int current = q.Dequeue();
+                Assert.AreEqual(1001-i, current);
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException), "Queue is empty.")]
+        public void DequeueFromEmptyqShouldThrowExeption()
+        {
+            LinkedQueue<int> q = new LinkedQueue<int>();
+
+            q.Dequeue();
+        }
+
+        [TestMethod]
+        public void TestqToArrayMethodShouldReturnSameArray()
+        {
+            LinkedQueue<int> q = new LinkedQueue<int>();
+            int[] arr = { 1, 2, 3, 4, 5 };
+            for (int i = 0; i < arr.Length; i++)
+            {
+                q.Enqueue(arr[i]);
+            }
+
+            int[] result = q.ToArray();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Assert.AreEqual(arr[i], result[i]);
+            }
+        }
+
+        [TestMethod]
+        public void TestToArrayMethodWithEmptyqShouldReturnEmptyArray()
+        {
+            LinkedQueue<int> q = new LinkedQueue<int>();
+
+            int[] result = q.ToArray();
+
+            Assert.AreEqual(0, result.Length);
+        }
+    }
+}
