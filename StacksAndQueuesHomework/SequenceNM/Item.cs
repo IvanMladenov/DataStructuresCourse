5d265a6e578cb2d0dfa2e16e19ba4bfd4ac4677f
IvanMladenov <ismladenov@gmail.com>
2015-11-17 15:06:27 +0200

    Sequence N->M problem

---
+++ StacksAndQueuesHomework/SequenceNM/Item.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.InteropServices.ComTypes;
+
+namespace SequenceNM
+{
+    public class Item
+    {
+        public Item(int value, Item previouse)
+        {
+            this.Value = value;
+            this.Previouse = previouse;
+        }
+
+        public Item Previouse { get; set; }
+
+        public int Value { get; set; }
+    }
+}
