f4473f5ca9ad678f0385829d53bcdc1e9c6c482f
IvanMladenov <ismladenov@gmail.com>
2015-11-17 13:34:12 +0200

    Custom ArrayStack and unit tests for it

---
+++ StacksAndQueuesHomework/ArrayStack/ArrayStack.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace ArrayStack
+{
+    public class ArrayStack<T>
+    {
+        private T[] elements;
+        private const int InitialCapacity = 16;
+
+        public ArrayStack()
+        {
+            elements = new T[InitialCapacity];
+        }
+
+        public int Count { get; private set; }
+
+        public void Push(T element)
+        {
+            if (elements.Length==Count)
+            {
+                Grow();
+            }
+
+            elements[Count] = element;
+            Count++;
+        }
+
+        public T Pop()
+        {
+            T element = elements[Count - 1];
+            Count--;
+            return element;
+        }
+
+        public T[] ToArray()
+        {
+            T[] arr = new T[Count];
+            for (int i = 0; i < Count; i++)
+            {
+                arr[i] = elements[i];
+            }
+            
+            Array.Reverse(arr);
+            return arr;
+        }
+
+        private void Grow()
+        {
+            T[] newArr = new T[2*elements.Length];
+            for (int i = 0; i < elements.Length; i++)
+            {
+                newArr[i] = elements[i];
+            }
+
+            elements = newArr;
+        }
+    }
+}
---
+++ StacksAndQueuesHomework/ArrayStackTests/TestMethods.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using ArrayStack;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ArrayStackTests
+{
+    [TestClass]
+    public class TestMethods
+    {
+        [TestMethod]
+        public void TestPushPopSingleElementShouldChangeCountPopTheSameElementLikePushed()
+        {
+            ArrayStack<int> stack = new ArrayStack<int>();
+            int number = 5;
+
+            int count = stack.Count;
+            Assert.AreEqual(count, 0);
+
+            stack.Push(number);
+            Assert.AreEqual(1, stack.Count);
+
+            int poped = stack.Pop();
+            Assert.AreEqual(number, poped);
+
+            Assert.AreEqual(0, stack.Count);
+        }
+
+        [TestMethod]
+        public void TestPushPopTousendElementsTheCountShouldBeCorrectIndirectlyTestAutoGrow()
+        {
+            ArrayStack<int> stack = new ArrayStack<int>();
+         
+            for (int i = 1; i <= 1000; i++)
+            {
+                Assert.AreEqual(i-1, stack.Count);
+                stack.Push(i);
+            }
+
+            for (int i = 1000; i >= 1; i--)
+            {
+                Assert.AreEqual(i, stack.Count);
+                int current = stack.Pop();
+                Assert.AreEqual(i, current);
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void PopFromEmptyStackShouldThrowExeption()
+        {
+            ArrayStack<int> stack = new ArrayStack<int>();
+
+            stack.Pop();
+        }
+
+        [TestMethod]
+        public void TestStackToArrayMethodShouldReturnSameArrayReversed()
+        {
+            ArrayStack<int> stack = new ArrayStack<int>();
+            int[] arr = {1, 2, 3, 4, 5};
+            for (int i = 0; i < arr.Length; i++)
+            {
+                stack.Push(arr[i]);
+            }
+
+            int[] result = stack.ToArray();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Assert.AreEqual(arr[i], result[arr.Length-1-i]);
+            }
+        }
+
+        [TestMethod]
+        public void TestToArrayMethodWithEmptyStackShouldReturnEmptyArray()
+        {
+            ArrayStack<int> stack = new ArrayStack<int>();
+
+            int[] result = stack.ToArray();
+
+            Assert.AreEqual(0, result.Length);
+        }
+    }
+}
