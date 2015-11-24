4cde9ee2eef24b24ef0ba6a8813de7b3288764db
IvanMladenov <ismladenov@gmail.com>
2015-11-24 18:24:50 +0200

    Arithmetic expression calculation, not so good formated but working for given examples (exept wrong expressions)

---
+++ TreesAndTreeLikeStructuresHomework/TreesAndTreeLikeStructures/Tree.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace TreesAndTreeLikeStructures
+{
+    class Tree<T>
+    {
+        public Tree(T value, params Tree<T>[] children )
+        {
+            Value = value;
+            Children = new List<Tree<T>>();
+            foreach (var child in children)
+            {
+                Children.Add(child);
+                child.Parent = this;
+            }
+        }
+
+        public T Value { get; set; }
+
+        public Tree<T> Parent { get; set; }
+
+        public IList<Tree<T>>  Children { get; set; }
+    }
+}
