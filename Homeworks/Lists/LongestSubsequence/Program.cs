9334d913bb84f7dce7b83a48301fb944f0ebc0a5
IvanMladenov <ismladenov@gmail.com>
2016-02-24 11:54:59 +0200

    add solution for sortWords and remove odd occurences

---
+++ Homeworks/Lists/LongestSubsequence/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LongestSubsequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> sequence = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+            int number = sequence[0];
+            int count = 1;
+            int currentNumber = sequence[0];
+            int currentCount = 1;
+
+            for (int i = 1; i < sequence.Count; i++)
+            {
+                if(sequence[i] == currentNumber)
+                {
+                    currentCount++;
+                }
+                else
+                {
+                    if (currentCount > count)
+                    {
+                        count = currentCount;
+                        number = currentNumber;
+                    }
+
+                    currentNumber = sequence[i];
+                    currentCount = 1;
+                }
+            }
+
+            if (currentCount > count)
+            {
+                count = currentCount;
+                number = currentNumber;
+            }
+
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write(number + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Homeworks/Lists/RemoveOddOccurences/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveOddOccurences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> sequence = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+            List<int> removed =  RemoveOddCountNumbers(sequence);
+
+            Console.WriteLine(string.Join(" ", removed));
+
+            
+        }
+
+        private static List<int> RemoveOddCountNumbers(List<int> list)
+        {
+            Dictionary<int, int> data = new Dictionary<int, int>();
+            List<int> output = new List<int>();
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (!data.ContainsKey(list[i]))
+                {
+                    data.Add(list[i], 0);
+                }
+
+                data[list[i]]++;
+            }
+
+            foreach (var number in list)
+            {
+                if (data[number] % 2 == 0)
+                {
+                    output.Add(number);
+                }
+            }
+
+            return output;
+        }
+    }
+}
