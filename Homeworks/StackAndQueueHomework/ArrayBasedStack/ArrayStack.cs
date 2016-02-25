275604bd57bb3b1c5ca50408002ab958ff2286a9
Ivan Mladenov <ismladenov@gmail.com>
2016-02-25 13:19:12 +0200

    implement array based stack, all works4

---
+++ Homeworks/StackAndQueueHomework/ArrayBasedStack/ArrayStack.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayBasedStack
+{
+    class ArrayStack<T>
+    {
+        private const int DefaultCapacity = 16;
+
+        private T[] elements;
+
+        public ArrayStack(int capacity = DefaultCapacity)
+        {
+            this.elements = new T[capacity];
+        }
+
+        public int  Count { get; set; }
+
+        public void Push(T element)
+        {
+            if (this.elements.Length == this.Count)
+            {
+                this.Grow();
+            }
+
+            this.elements[this.Count] = element;
+            this.Count++;
+        }
+
+        public T Pop()
+        {
+            if (this.Count==0)
+            {
+                throw new InvalidOperationException("The stack is empty");
+            }
+
+            this.Count--;
+            return this.elements[this.Count];
+        }
+
+        public T[] ToArray()
+        {
+            var arr = new T[this.Count];
+            Array.Copy(this.elements, arr, this.Count);
+
+            return arr;
+        }
+
+        private void Grow()
+        {
+            var newArray = new T[this.elements.Length * 2];
+            Array.Copy(this.elements, newArray, this.elements.Length);
+
+            this.elements = newArray;
+        }
+    }
+}
---
+++ Homeworks/StackAndQueueHomework/ArrayBasedStack/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayBasedStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new ArrayStack<int>();
+
+            for (int i = 0; i < 50; i++)
+            {
+                stack.Push(i);
+            }
+
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Count);
+            Console.WriteLine(stack.Pop());
+        }
+    }
+}
