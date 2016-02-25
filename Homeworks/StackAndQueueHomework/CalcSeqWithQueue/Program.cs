65ff70a0d84302046b40828b995a97619171d3d9
Ivan Mladenov <ismladenov@gmail.com>
2016-02-25 12:07:36 +0200

    add first two problems of queues and stacks homework

---
+++ Homeworks/StackAndQueueHomework/CalcSeqWithQueue/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace CalcSeqWithQueue
+{
+    using System;
+    using System.Collections.Generic;
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var queue = new Queue<int>();
+            queue.Enqueue(n);
+
+            for (int i = 0; i < 50; i++)
+            {
+                var firstInQueue = queue.Peek();
+
+                queue.Enqueue(firstInQueue + 1);
+                queue.Enqueue(2 * firstInQueue + 1);
+                queue.Enqueue(firstInQueue + 2);
+
+                Console.Write(queue.Dequeue() + " ");
+            }
+        }
+    }
+}
---
+++ Homeworks/StackAndQueueHomework/ReverseNumbersWithStack/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ReverseNumbersWithStack
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    internal class Program
+    {
+        private static void Main()
+        {
+            Stack<int> stack = new Stack<int>();
+
+            var numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            Console.WriteLine(string.Join(" ", numbers));
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                stack.Push(numbers[i]);
+            }
+
+            Console.WriteLine(string.Join(" ", stack));
+        }
+    }
+}
